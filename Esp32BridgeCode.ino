378a257c35c9490293f4da95b5d7a0582f3a4036
Surya Bajpai <suryamanibajpai@gmail.com>
2026-09-23 16:20:19 +0530

    Update Esp32BridgeCode.ino
    Added comment to clarify UART configuration.

--- Esp32BridgeCode.ino
+++ Esp32BridgeCode.ino
@@ -6,7 +6,7 @@ HardwareSerial sim800(2);   // UART2
 void setup()
 {
   Serial.begin(115200);                         // USB Serial Monitor
-  sim800.begin(9600, SERIAL_8N1, 16, 17);       // RX2, TX2
+  sim800.begin(9600, SERIAL_8N1, 16, 17);       // RX2, TX2 ( 8 Data Bits ,No Parity, 1 Stop Bit)
   delay(3000);
   Serial.println("ESP32 SIM800 Bridge Ready");
 }
